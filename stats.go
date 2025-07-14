ff420c225614b05efd9030f022ee433bb363bb9b
aomori168 <aomori446@yahoo.co.jp>
2025-07-14 10:35:57 +0900

    update stats.go

--- stats.go
+++ stats.go
@@ -1,10 +1,8 @@
 package pool
 
-import "sync/atomic"
-
 // Stats 包含工作池操作的統計數據。
 type Stats struct {
-	pending   atomic.Int64
-	completed atomic.Int64
-	failed    atomic.Int64
+	Pending   int64
+	Completed int64
+	Failed    int64
 }
